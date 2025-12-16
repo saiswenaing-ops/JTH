c0719a137e342f7098b3e5ea6536a01176adf67b
ssnh <saiswenaing@gmail.com>
2025-12-16 21:49:39 +0630

    test 360

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pxl_20251216_131728228photosphere",
+      "name": "PXL_20251216_131728228.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.968084888263384,
+        "pitch": 0.11773108664903731,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.23501551826004,
+          "pitch": 0.16978367797018556,
+          "rotation": 1.5707963267948966,
+          "target": "1-pxl_20251216_153411561photosphere"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pxl_20251216_153411561photosphere",
+      "name": "PXL_20251216_153411561.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08355510416256351,
+        "pitch": 0.026182322212957132,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7668144527182141,
+          "pitch": 0.13314238424916347,
+          "rotation": 3.141592653589793,
+          "target": "0-pxl_20251216_131728228photosphere"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cinema &JC Lounge ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
